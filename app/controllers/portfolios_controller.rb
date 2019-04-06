66b1458ba9785e1f3268317b98d184ac2a0aa3ba
JuanJo Rodriguez <juanjoserc@gmail.com>
2019-04-06 11:36:03 -0600

    Implemented portfolio edit and update methods

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -19,6 +19,20 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'Record was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
 
   private
     # Never trust parameters from the scary internet, only allow the white list through.
