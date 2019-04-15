0eadab47aaa5b5cbb8f0c05ca3dc79215ffa7f12
JuanJo Rodriguez <juanjoserc@gmail.com>
2019-04-15 09:08:26 -0500

    Implemented detroy action for portfolio feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -37,6 +37,17 @@ class PortfoliosController < ApplicationController
   def show
     @portfolio_item = Portfolio.find(params[:id])
   end
+
+  def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+
+    @portfolio_item.destroy
+
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Portfolio was successfully destroyed.' }
+    end
+  end
+
   private
     # Never trust parameters from the scary internet, only allow the white list through.
     def portfolio_params
