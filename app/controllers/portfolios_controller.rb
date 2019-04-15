be0fe81225345df8746f27f3fb78873d23e46094
JuanJo Rodriguez <juanjoserc@gmail.com>
2019-04-15 08:53:22 -0500

    Implemented show functionality for portfolio module

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -34,6 +34,9 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
+  end
   private
     # Never trust parameters from the scary internet, only allow the white list through.
     def portfolio_params
